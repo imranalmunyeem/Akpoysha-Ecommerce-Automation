85826d32c0665914a49d3990eb7377b3ac8f7eaa
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2022-01-27 17:35:40 +0600

    search

---
+++ cypress/integration/006_search.spec.js
@@ -0,0 +1,12 @@
+///<reference types = 'cypress' />
+
+describe('Search', ()=>{
+    it.only('Search featured should provide correct output with valid data', ()=>{
+        cy.visit('/');
+    })
+
+
+    it('Search featured should not provide correct output with invalid data', ()=>{
+        
+    })
+})

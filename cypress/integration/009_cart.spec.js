8a045834d7fbdbdca3c954cc8c3671390b2ccf21
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2022-01-28 19:56:20 +0600

    cart

---
+++ cypress/integration/009_cart.spec.js
@@ -0,0 +1,32 @@
+///<reference types = 'cypress'/>
+
+import SearchPO from "../support/page_objects/SearchPO";
+
+ describe('Cart Section', ()=>{
+     it('Navigate to login', ()=>{
+        cy.login('ialmunyeem@gmail.com', 'akpoysha007'); //running from command
+        
+     })
+     it('Search for product and click on it', ()=>{
+         const searchpo = new SearchPO();
+         searchpo.clickOnSearch('chicken breast');
+        cy.get('.autocomplete-suggestion').click();
+     })
+
+     //N.B: didn't create any page object for cart feature since the products will keep changing
+    //  it('Select Quantity', ()=>{
+    //     cy.get('.cart > .quantity > .plus').click();
+    //  })
+
+    //  it('Click on Add to cart', ()=>{
+    //     cy.get('.single_add_to_cart_button').click();
+    //  })
+
+    //  it('Click on View Cart', ()=>{
+    //     cy.get('.btn-cart').click();
+    //  })
+
+    //  it('Click on procceed to checkout', ()=>{
+    //     cy.get('.checkout-button').click();
+    //  })
+ })
